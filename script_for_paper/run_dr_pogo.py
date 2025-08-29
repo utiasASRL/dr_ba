39e70a6b16eeab0233d55e4402b4e51d9f7a8e98
Cedric Le Gentil <le.gentil.cedric@gmail.com>
2025-08-29 16:48:37 -0400

    Modify script to run on old boreas

--- script_for_paper/run_dr_pogo.py
+++ script_for_paper/run_dr_pogo.py
@@ -6,26 +6,43 @@ import subprocess
 import yaml
 
 
+## RSS data
+#sequences = [
+#             'boreas-2024-12-05-14-12', # Commercial
+#             'boreas-2024-12-23-16-27', # Commercial
+#             'boreas-2024-12-23-16-44', # Commercial
+#             'boreas-2024-12-23-17-01', # Commercial
+#             'boreas-2024-12-23-17-18', # Commercial
+#
+#
+#             'boreas-2024-12-03-10-24', # Glenshield
+#             'boreas-2024-12-03-12-54', # Glenshield
+#             'boreas-2025-01-08-10-59', # Glenshield
+#             'boreas-2025-01-08-11-22', # Glenshield
+#             'boreas-2025-01-08-12-28', # Glenshield
+#
+#             'boreas-2024-12-04-11-45', # Skyway
+#             'boreas-2024-12-04-11-56', # Skyway
+#             'boreas-2024-12-04-12-08', # Skyway
+#             'boreas-2024-12-04-12-19', # Skyway
+#             'boreas-2024-12-04-12-34', # Skyway
+#            ]
 
+## Original Boreas data
 sequences = [
-             'boreas-2024-12-05-14-12', # Commercial
-             'boreas-2024-12-23-16-27', # Commercial
-             'boreas-2024-12-23-16-44', # Commercial
-             'boreas-2024-12-23-17-01', # Commercial
-             'boreas-2024-12-23-17-18', # Commercial
-
-
-             'boreas-2024-12-03-10-24', # Glenshield
-             'boreas-2024-12-03-12-54', # Glenshield
-             'boreas-2025-01-08-10-59', # Glenshield
-             'boreas-2025-01-08-11-22', # Glenshield
-             'boreas-2025-01-08-12-28', # Glenshield
-
-             'boreas-2024-12-04-11-45', # Skyway
-             'boreas-2024-12-04-11-56', # Skyway
-             'boreas-2024-12-04-12-08', # Skyway
-             'boreas-2024-12-04-12-19', # Skyway
-             'boreas-2024-12-04-12-34', # Skyway
+            'boreas-2020-11-26-13-58',
+            'boreas-2020-12-18-13-44',
+            'boreas-2021-01-26-11-22',
+            'boreas-2021-02-02-14-07',
+            'boreas-2021-03-02-13-38',
+            'boreas-2021-03-30-14-23',
+            'boreas-2021-04-20-14-11',
+            'boreas-2021-05-13-16-11',
+            'boreas-2021-07-20-17-33',
+            'boreas-2021-09-02-11-42',
+            'boreas-2021-10-15-12-35',
+            'boreas-2021-11-14-09-47',
+            'boreas-2021-11-23-14-27',
             ]
 
 data_path = utils.getDataDir()
