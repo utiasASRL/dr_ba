165da53938c414ec5efe437e4ac18bc3cbbdd52e
Daniil <lisus.daniil@gmail.com>
2026-01-12 20:12:51 -0500

    Improved relative state loading

--- ba/app/dr_ba.cpp
+++ ba/app/dr_ba.cpp
@@ -16,11 +16,6 @@
 #include <Eigen/Sparse>
 
 namespace fs = std::filesystem;
-using SpMat = Eigen::SparseMatrix<double>;
-using Triplet = Eigen::Triplet<double>;
-using Vec = Eigen::VectorXd;
-using Vec3d = Eigen::Vector3d;
-using Mat = Eigen::MatrixXd;
 
 int main() {
     // Load in config from ba/config/dr_ba_config.yaml
@@ -71,9 +66,9 @@ int main() {
     std::sort(cumul_files.begin(), cumul_files.end());
 
     // Initialize looping through trajectory
-    lgmath::se3::Transformation T_gt_0;
-    lgmath::se3::Transformation T_est_0;
-    lgmath::se3::Transformation T_kf_prev;  // Previous keyframe pose
+    lgmath::se3::Transformation T_gt_abs_0(Eigen::Matrix4d(Eigen::Matrix4d::Identity()));
+    lgmath::se3::Transformation T_est_abs_0(Eigen::Matrix4d(Eigen::Matrix4d::Identity()));
+    lgmath::se3::Transformation T_kf_prev(Eigen::Matrix4d(Eigen::Matrix4d::Identity()));  // Previous keyframe pose
     int kf_prev_id = 0;
     ankerl::unordered_dense::map<std::pair<int32_t, int32_t>, lgmath::se3::Transformation> pose_priors;
 
@@ -90,26 +85,29 @@ int main() {
         double timestamp_scan = std::stod(path.stem().string()) / 1e6; // convert to seconds
 
         // Load in gt pose
-        lgmath::se3::Transformation T_gt = ba::get_interpolated_pose(all_gt_poses, all_gt_times, timestamp_scan);
+        lgmath::se3::Transformation T_gt_abs = ba::get_interpolated_pose(all_gt_poses, all_gt_times, timestamp_scan);
 
         // Load in initial guess pose
-        lgmath::se3::Transformation T_est;
+        lgmath::se3::Transformation T_est_rel(Eigen::Matrix4d(Eigen::Matrix4d::Identity()));
+        lgmath::se3::Transformation T_est_abs;
         if (opts.init_poses == "pogo") {
-            T_est = ba::get_interpolated_pose(all_pogo_poses, all_pogo_times, timestamp_scan);
+            T_est_abs = ba::get_interpolated_pose(all_pogo_poses, all_pogo_times, timestamp_scan);
+            T_est_rel = T_est_abs_0.inverse() * T_est_abs;
         } else if (opts.init_poses == "gt") {
-            T_est = ba::get_interpolated_pose(all_gt_poses, all_gt_times, timestamp_scan);
+            T_est_abs = ba::get_interpolated_pose(all_gt_poses, all_gt_times, timestamp_scan);
+            T_est_rel = T_est_abs_0.inverse() * T_est_abs;
             // Add noise to gt pose sampled from uniform distribution
-            Vec3d noise;
+            Eigen::Vector3d noise;
             noise << translation_dist(rng), translation_dist(rng), rotation_dist(rng);
             lgmath::se3::Transformation T_noise = lgmath::se2::Transformation(noise).toSE3();
-            T_est = T_est * T_noise;            
+            T_est_rel = T_est_rel * T_noise;
         } else {
             throw std::invalid_argument("Invalid init_poses option: " + opts.init_poses);
         }
 
         if (num_loaded != 0) {
             // Check if this pose is a keyframe
-            lgmath::se3::Transformation T_kf_rel = T_est.inverse() * T_kf_prev;
+            lgmath::se3::Transformation T_kf_rel = T_est_abs.inverse() * T_kf_prev;
             double del_x = T_kf_rel.r_ab_inb()(0);
             double del_y = T_kf_rel.r_ab_inb()(1);
             double del_theta = T_kf_rel.vec()(5); // Yaw angle
@@ -122,22 +120,20 @@ int main() {
             // Set up prior from prev keyframe radar frame to this keyframe radar frame
             pose_priors[{kf_prev_id, num_checked}] = T_kf_rel;
         } else {
-            T_gt_0 = T_gt;
-            T_est_0 = T_est;
-            T_kf_prev = T_est;
+            T_gt_abs_0 = T_gt_abs;
+            T_est_abs_0 = T_est_abs;
+            T_kf_prev = T_est_abs;
+            T_est_rel = lgmath::se3::Transformation(Eigen::Matrix4d(Eigen::Matrix4d::Identity()));
         }
 
         // We've decided this is a keyframe!
         std::cout << "Processing frame " << num_checked << " / " << num_scans << std::endl;
         kf_prev_id = num_checked;
         num_loaded++;
-        T_kf_prev = T_est; 
+        T_kf_prev = T_est_abs;
 
         // Get relative gt transform
-        lgmath::se3::Transformation T_gt_rel = T_gt_0.inverse() * T_gt;
-
-        // Get relative est transform
-        lgmath::se3::Transformation T_est_rel = T_est_0.inverse() * T_est;
+        lgmath::se3::Transformation T_gt_rel = T_gt_abs_0.inverse() * T_gt_abs;
 
         // TODO: Add support for more than just local_maps
         if (opts.input_type != "local_maps") {
@@ -153,7 +149,7 @@ int main() {
         }
         // Convert to CV_64F and normalize to [0, 1]
         img.convertTo(img, CV_64F, 1.0 / 255.0);
-        Mat img_mat;
+        Eigen::MatrixXd img_mat;
         cv::cv2eigen(img, img_mat);
 
         // Load in cumulative return image
@@ -161,8 +157,12 @@ int main() {
         cv::Mat cumul_img = cv::imread(cumul_path.string(), cv::IMREAD_GRAYSCALE);
         // Convert to CV_64F and normalize to [0, 1]
         cumul_img.convertTo(cumul_img, CV_64F, 1.0 / 255.0);
-        Mat cumul_img_mat;
+        Eigen::MatrixXd cumul_img_mat;
         cv::cv2eigen(cumul_img, cumul_img_mat);
+
+        // Project relative matrices to SE2
+        T_est_rel = T_est_rel.toSE2().toSE3();
+        T_gt_rel = T_gt_rel.toSE2().toSE3();
 
         // Create scan object
         auto scan = std::make_shared<ba::LocalMapScan>(num_checked, opts, T_est_rel, T_gt_rel, img_mat, cumul_img_mat);
