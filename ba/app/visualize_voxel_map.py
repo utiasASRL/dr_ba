4cbc11caaaf356b126581e429a01074129470ad6
Daniil <lisus.daniil@gmail.com>
2026-01-15 16:09:01 -0500

    Updated visualization to plot pose errors

--- ba/app/visualize_voxel_map.py
+++ ba/app/visualize_voxel_map.py
@@ -1,9 +1,11 @@
 import struct
 import matplotlib.pyplot as plt
 import numpy as np
+import os.path as osp
 
-path =  '/home/dl/Documents/phd/dev/dr_ba/output/run_2/voxels.bin'
-with open(path, "rb") as f:
+base_path =  '/home/dl/Documents/phd/dev/dr_ba/output/ba_results/run_38'
+voxel_path = osp.join(base_path, 'voxel_map.bin')
+with open(voxel_path, "rb") as f:
     res, = struct.unpack("d", f.read(8))
 
     voxels = []
@@ -27,4 +29,28 @@ plt.xlabel('X (m)')
 plt.ylabel('Y (m)')
 plt.title('Voxel Map Visualization')
 plt.axis('equal')
+
+# Load in poses and color by error if they exist
+pose_path = osp.join(base_path, 'scan_poses.csv')
+if osp.exists(pose_path):
+    # scan_id, x, y, yaw, x_gt, y_gt, yaw_gt
+    data = np.loadtxt(pose_path, delimiter=",", skiprows=1)
+    scan_ids = data[:, 0].astype(int)
+    x_est = data[:,1] - data[0,1]
+    y_est = data[:,2] - data[0,2]
+    yaw_est = data[:,3] - data[0,3]
+    x_gt = data[:,4] - data[0,4]
+    y_gt = data[:,5] - data[0,5]
+    yaw_gt = data[:,6] - data[0,6]
+
+    trans_errors = np.sqrt((x_est - x_gt)**2 + (y_est - y_gt)**2)
+    yaw_errors = np.abs(yaw_est - yaw_gt) * (180.0 / np.pi)
+
+    print("RMSE Translational Error (m):", np.sqrt(np.mean(trans_errors**2)))
+    print("RMSE Rotational Error (deg):", np.sqrt(np.mean(yaw_errors**2)))
+
+    sc = plt.scatter(x_est, y_est, c=trans_errors, cmap='hot', s=20, edgecolors='k', label='Estimated Poses')
+    plt.colorbar(sc, label='Translational Error (m)')
+    plt.legend()
+
 plt.show()
